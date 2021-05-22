8c4f143909ecb398890d5202e98b370bfa1cceef
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-05-22 16:22:29 +0900

    mdxをページにできるようにする

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 // MDXを読み込めるようにする
 // ref: https://www.npmjs.com/package/@next/mdx
-const withMDX = require("@next/mdx")();
-module.exports = withMDX();
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx$/,
+});
+module.exports = withMDX({
+  pageExtensions: ["tsx", "mdx"],
+  future: {
+    webpack5: true,
+  },
+});
