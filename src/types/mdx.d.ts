0b599b238cf9e9cdba83f881168b480d4932bb96
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-05-27 22:37:01 +0900

    d.tsの場所を変える

--- src/types/mdx.d.ts
+++ src/types/mdx.d.ts
@@ -1,6 +1,6 @@
 declare module "*.mdx" {
   import type { ComponentType } from "react";
-  import type { FrontMatter } from "../types/FrontMatter";
+  import type { FrontMatter } from "./FrontMatter";
   const MDXComponent: ComponentType;
   export const frontMatter: FrontMatter;
   export default MDXComponent;
