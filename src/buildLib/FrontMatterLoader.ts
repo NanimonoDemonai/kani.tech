fe1cd96b6f6f75dceda724661a6e83173e9f1186
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-05-27 22:17:17 +0900

    FrontMatterのloaderを作る

---
+++ src/buildLib/FrontMatterLoader.ts
@@ -0,0 +1,17 @@
+import { loader } from "webpack";
+import parser from "gray-matter";
+import stringifyObject from "stringify-object";
+import { unknownObjectToFrontMatter } from "../utils/FrontMatterParser";
+export const FMLoader: loader.Loader = function (src) {
+  const callback = this.async();
+  const { data, content } = parser(src);
+  const frontMatter = unknownObjectToFrontMatter(data);
+  console.log(frontMatter);
+  const code = [
+    `export const frontMatter = ${stringifyObject(frontMatter)}`,
+    "",
+    content,
+  ].join("\n");
+
+  if (callback) return callback(null, code);
+};
