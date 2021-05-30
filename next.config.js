22a38883a07dbc2cf9adb82f856c2982a5755f41
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-05-30 18:02:47 +0900

    loaderを描き直す

--- next.config.js
+++ next.config.js
@@ -17,11 +17,7 @@ module.exports = {
     );
     config.module.rules.push({
       test: /\.mdx$/,
-      use: [
-        "babel-loader",
-        "@mdx-js/loader",
-        path.join(__dirname, "./src/buildLib/fmLoader.js"),
-      ],
+      use: ["babel-loader", path.join(__dirname, "./src/buildLib/fmLoader.js")],
     });
     return config;
   },
--- src/buildLib/FrontMatterLoader.ts
+++ src/buildLib/FrontMatterLoader.ts
@@ -2,16 +2,24 @@ import { loader } from "webpack";
 import parser from "gray-matter";
 import stringifyObject from "stringify-object";
 import { unknownObjectToFrontMatter } from "./FrontMatterParser";
+const mdx: (content: string) => Promise<string> = require("@mdx-js/mdx");
+// language=js
+const DEFAULT_RENDERER = `
+import React from 'react'
+import {mdx} from '@mdx-js/react'
+`;
+
 export const FMLoader: loader.Loader = function (src) {
   const callback = this.async();
   const { data, content } = parser(src);
   const frontMatter = unknownObjectToFrontMatter(data);
-  console.log(frontMatter);
-  const code = [
-    `export const frontMatter = ${stringifyObject(frontMatter)}`,
-    "",
-    content,
-  ].join("\n");
-
-  if (callback) return callback(null, code);
+  mdx(content).then((jsx) => {
+    const code = [
+      DEFAULT_RENDERER,
+      // language=js
+      `export const frontMatter = ${stringifyObject(frontMatter)}`,
+      jsx,
+    ].join("\n");
+    if (callback) return callback(null, code);
+  });
 };
