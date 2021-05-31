5c6f8b72e17a2c34185ac567c4401dd522da7660
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-01 00:00:23 +0900

    抑制する

--- src/types/HTMLAttributes.d.ts
+++ src/types/HTMLAttributes.d.ts
@@ -1,5 +1,6 @@
 import { AtomicProps } from "@fower/types";
 
 declare module "react" {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-interface
   interface HTMLAttributes<T> extends AtomicProps {}
 }
