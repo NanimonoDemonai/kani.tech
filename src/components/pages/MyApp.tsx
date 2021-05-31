1f209dede8d7def9f2101f97918e266bb1ddae30
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-01 01:23:13 +0900

    @fower/reactをimportしておく

--- src/components/pages/MyApp.tsx
+++ src/components/pages/MyApp.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import { PageNavbar } from "../Navbar/PageNavbar";
 import "normalize.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
+import "@fower/react";
 
 export const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
   <div id="wrapper-wrapper" minH52>
