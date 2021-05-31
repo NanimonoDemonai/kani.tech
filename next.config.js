be9b68ce7ef86fb7a90788c7ec5a195bb96891b4
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-05-31 22:31:46 +0900

    fowerに移行する

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
-const WindiCSSWebpackPlugin = require("windicss-webpack-plugin").default;
 const path = require("path");
 
 module.exports = {
   pageExtensions: ["tsx"],
   future: {
-    webpack5: false,
+    webpack5: true,
   },
   webpack: (config) => {
-    config.plugins.push(
-      new WindiCSSWebpackPlugin({
-        scan: {
-          dirs: ["./"],
-          exclude: ["node_modules", ".git", ".next/**/*"],
-        },
-      })
-    );
     config.module.rules.push({
       test: /\.mdx$/,
       use: ["babel-loader", path.join(__dirname, "./src/buildLib/fmLoader.js")],
