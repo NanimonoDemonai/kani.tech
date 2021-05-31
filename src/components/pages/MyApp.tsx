ac2759849dd66f097a93ea4a73c40aa7f64c2e46
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-01 00:14:30 +0900

    AppTsxを分ける

---
+++ src/components/pages/MyApp.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import { AppProps } from "next/app";
+import { PageNavbar } from "../Navbar/PageNavbar";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+
+export const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
+  <div id="wrapper-wrapper" minH52>
+    <PageNavbar />
+    <div id="wrapper" row toCenterX>
+      <main p5 textLG maxW--lg="70%" w="100%">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  </div>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,3 @@
-import "normalize.css";
-import "@blueprintjs/core/lib/css/blueprint.css";
-import type { AppProps } from "next/app";
-import { VFC } from "react";
-import { PageNavbar } from "../components/Navbar/PageNavbar";
-import { Box } from "@fower/react";
-
-const MyApp: VFC<AppProps> = ({ Component, pageProps }) => (
-  <Box minH52>
-    <PageNavbar />
-    <div id="wrapper" row toCenterX>
-      <main p5 textLG maxW--lg="70%" w="100%">
-        <Component {...pageProps} />
-      </main>
-    </div>
-  </Box>
-);
+import { MyApp } from "../components/pages/MyApp";
 
 export default MyApp;
