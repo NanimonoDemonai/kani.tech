8583c7d6af78bc9fad52196f8139d672ca64f267
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 02:32:59 +0900

    title設定でatomを更新するようにする

--- src/components/Metas/Title.tsx
+++ src/components/Metas/Title.tsx
@@ -1,11 +1,20 @@
-import { VFC } from "react";
+import { useEffect, VFC } from "react";
 import Head from "next/head";
 import { SITE_NAME } from "../../constants/envNames";
+import { useRecoilState } from "recoil";
+import { titleAtoms } from "../../atoms/title";
 
-export const Title: VFC<{ title: string }> = ({ title }) => (
-  <Head>
-    <title>
-      {title} | {SITE_NAME}
-    </title>
-  </Head>
-);
+export const Title: VFC<{ title: string }> = ({ title }) => {
+  const setTitle = useRecoilState(titleAtoms)[1];
+  useEffect(() => {
+    console.log(title);
+    setTitle(title);
+  }, [title]);
+  return (
+    <Head>
+      <title>
+        {title} | {SITE_NAME}
+      </title>
+    </Head>
+  );
+};
