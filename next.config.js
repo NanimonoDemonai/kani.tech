428690b4a74fec07f5601c8a57f8d0d91dbb355b
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-05 16:38:41 +0900

    fowerを消す

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-const path = require("path");
-
 module.exports = {
   pageExtensions: ["tsx"],
   future: {
     webpack5: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.mdx$/,
-      use: ["babel-loader", path.join(__dirname, "./src/buildLib/fmLoader.js")],
-    });
-    return config;
   },
 };
