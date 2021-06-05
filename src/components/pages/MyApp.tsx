6a723715f8edba9781d3bf403037816f9cb07075
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 02:50:37 +0900

    フッターを入れる

--- src/components/pages/MyApp.tsx
+++ src/components/pages/MyApp.tsx
@@ -3,14 +3,16 @@ import { AppProps } from "next/app";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import { PageNavbar } from "../Navbar/PageNavbar";
 import { RecoilRoot } from "recoil";
+import { Footer } from "../Footer/Footer";
 
 export const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
   <RecoilRoot>
     <ChakraProvider>
       <PageNavbar />
-      <Container maxW="container.lg" fontSize="lg" p={2}>
+      <Container maxW="container.lg" fontSize="lg" p={2} minH={"4xl"}>
         <Component {...pageProps} />
       </Container>
+      <Footer />
     </ChakraProvider>
   </RecoilRoot>
 );
