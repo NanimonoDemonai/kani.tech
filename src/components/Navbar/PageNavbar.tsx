30512fc115dd2b704019e1e72a7ea33699220634
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 02:34:23 +0900

    Navbarにタイトルを出す

--- src/components/Navbar/PageNavbar.tsx
+++ src/components/Navbar/PageNavbar.tsx
@@ -1,25 +1,40 @@
 import { VFC } from "react";
 import NextLink from "next/link";
-import { Center, HStack, Link, Spacer, StackDivider } from "@chakra-ui/react";
+import { Heading, HStack, Link, Spacer } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { KaniButton } from "./KaniButton";
 import { CODE_REPOSITORY, SITE_NAME } from "../../constants/envNames";
+import { useRecoilState } from "recoil";
+import { titleAtoms } from "../../atoms/title";
+import { NavBarDivider } from "./NavBarDivider";
 
-export const PageNavbar: VFC = () => (
-  <HStack borderColor="gray.200" shadow="md" p={2} px={30} mb={3}>
-    <KaniButton />
-    <NextLink href="/">
-      <Link>{SITE_NAME}</Link>
-    </NextLink>
-    <Center h="5">
-      <StackDivider borderLeft="1px" borderLeftColor="gray.200" />
-    </Center>
-    <NextLink href="/entries">
-      <Link>Entries</Link>
-    </NextLink>
-    <Spacer />
-    <Link href={CODE_REPOSITORY.toString()} isExternal>
-      Github <ExternalLinkIcon mx="2px" />
-    </Link>
-  </HStack>
-);
+export const PageNavbar: VFC = () => {
+  const [title] = useRecoilState(titleAtoms);
+
+  return (
+    <HStack borderColor="gray.200" shadow="md" p={2} px={30} mb={3}>
+      <header>
+        <HStack>
+          <KaniButton />
+          <NextLink href="/">
+            <Link>{SITE_NAME}</Link>
+          </NextLink>
+        </HStack>
+      </header>
+      <NavBarDivider />
+      <Heading fontSize="sm">{title}</Heading>
+      <Spacer />
+      <nav>
+        <HStack>
+          <NextLink href="/entries">
+            <Link>Entries</Link>
+          </NextLink>
+          <NavBarDivider />
+          <Link href={CODE_REPOSITORY.toString()} isExternal>
+            Github <ExternalLinkIcon mx="2px" />
+          </Link>
+        </HStack>
+      </nav>
+    </HStack>
+  );
+};
