8406d22fe99773440b34e846036efa656d328646
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 01:21:36 +0900

    ページ名を定数化

--- src/components/Metas/Title.tsx
+++ src/components/Metas/Title.tsx
@@ -1,8 +1,11 @@
 import { VFC } from "react";
 import Head from "next/head";
+import { SITE_NAME } from "../../constants/envNames";
 
 export const Title: VFC<{ title: string }> = ({ title }) => (
   <Head>
-    <title>{title} | kani.tech</title>
+    <title>
+      {title} | {SITE_NAME}
+    </title>
   </Head>
 );
--- src/components/Navbar/PageNavbar.tsx
+++ src/components/Navbar/PageNavbar.tsx
@@ -3,12 +3,13 @@ import NextLink from "next/link";
 import { Center, HStack, Link, StackDivider } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { KaniButton } from "./KaniButton";
+import { CODE_REPOSITORY, SITE_NAME } from "../../constants/envNames";
 
 export const PageNavbar: VFC = () => (
   <HStack borderColor="gray.200" shadow="md" p={2} mb={3}>
     <KaniButton />
     <NextLink href="/">
-      <Link>Kani.tech</Link>
+      <Link>{SITE_NAME}</Link>
     </NextLink>
     <Center h="5">
       <StackDivider borderLeft="1px" borderLeftColor="gray.200" />
@@ -16,7 +17,7 @@ export const PageNavbar: VFC = () => (
     <NextLink href="/entries">
       <Link>Entries</Link>
     </NextLink>
-    <Link href="https://github.com/NanimonoDemonai/kani.tech" isExternal>
+    <Link href={CODE_REPOSITORY.toString()} isExternal>
       Github <ExternalLinkIcon mx="2px" />
     </Link>
   </HStack>
