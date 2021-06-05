99e579371f0209219befdd96a830e37a91b00ccf
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 02:33:16 +0900

    RecoilRootをappに仕込む

--- src/components/pages/MyApp.tsx
+++ src/components/pages/MyApp.tsx
@@ -2,12 +2,15 @@ import { NextPage } from "next";
 import { AppProps } from "next/app";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import { PageNavbar } from "../Navbar/PageNavbar";
+import { RecoilRoot } from "recoil";
 
 export const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
-  <ChakraProvider>
-    <PageNavbar />
-    <Container maxW="container.lg" fontSize="lg" p={2}>
-      <Component {...pageProps} />
-    </Container>
-  </ChakraProvider>
+  <RecoilRoot>
+    <ChakraProvider>
+      <PageNavbar />
+      <Container maxW="container.lg" fontSize="lg" p={2}>
+        <Component {...pageProps} />
+      </Container>
+    </ChakraProvider>
+  </RecoilRoot>
 );
