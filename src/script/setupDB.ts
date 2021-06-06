04bc1d7193d86cef527622a6174e7948280b0431
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-07 00:16:30 +0900

    mdxをprisma経由でとってくる

--- src/script/setupDB.ts
+++ src/script/setupDB.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { PrismaClient } from "@prisma/client";
 import { getEntryPagePathList } from "../constants/EntryPageList";
+import { frontMatterParser } from "../utils/parsers/FrontMatterParser";
 import { readFileWithModifiedTime } from "../utils/readFileWithModifiedTime";
 
 const prisma = new PrismaClient();
@@ -10,9 +11,11 @@ async function main() {
   const entryPagePathList = await getEntryPagePathList();
   const dataReq = entryPagePathList.map((e) => readFileWithModifiedTime(e));
   const data = await Promise.all(dataReq);
+
   const result = data.map((e, i) => ({
     source: e.src,
     pageName: path.basename(entryPagePathList[i], ".mdx"),
+    pageTitle: frontMatterParser(e.src).frontMatter.title,
     modified: e.modified,
   }));
   const tableReq = result.map((e) => prisma.entry.create({ data: e }));
--- src/services/getMDXSourcePageCodeAndPageMetaWithPID.ts
+++ src/services/getMDXSourcePageCodeAndPageMetaWithPID.ts
@@ -1,5 +1,4 @@
 import { PageMeta } from "../types/PageMeta";
-import { sourceParser } from "../utils/parsers/sourceParser";
 import { prisma } from "./client/PrismClient";
 
 interface Res {
@@ -11,12 +10,11 @@ export const getMDXSourcePageCodeAndPageMetaWithPID = async (
 ): Promise<Res | undefined> => {
   const entry = await prisma.entry.findUnique({ where: { pageName } });
   if (!entry) return;
-  const { frontMatter } = await sourceParser(entry.source);
   return {
     pageMeta: {
       modified: entry.modified.toJSON(),
       source: entry.source,
-      title: frontMatter.title,
+      title: entry.pageTitle,
     },
   };
 };
