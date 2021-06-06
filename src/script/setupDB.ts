ab2deb9a7da3d822931350ef79cc658854c0c7ed
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 23:20:38 +0900

    データベースの初期化をやる

---
+++ src/script/setupDB.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { PrismaClient } from "@prisma/client";
+import { getEntryPagePathList } from "../constants/EntryPageList";
+import { readFileWithModifiedTime } from "../utils/readFileWithModifiedTime";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.entry.deleteMany({});
+  const entryPagePathList = await getEntryPagePathList();
+  const dataReq = entryPagePathList.map((e) => readFileWithModifiedTime(e));
+  const data = await Promise.all(dataReq);
+  const result = data.map((e, i) => ({
+    source: e.src,
+    pageName: path.basename(entryPagePathList[i], ".mdx"),
+    modified: e.modified,
+  }));
+  const tableReq = result.map((e) => prisma.entry.create({ data: e }));
+  const table = await Promise.all(tableReq);
+  console.log(table);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
