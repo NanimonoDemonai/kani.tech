53b4bc4c13aa6217f98bafb7e7086bfebfa90dae
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 23:34:13 +0900

    エントリーリストをprisma経由でとる

---
+++ src/services/getEntryPageListFromLocal.ts
@@ -0,0 +1,19 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { getEntryPagePathList } from "../constants/EntryPageList";
+
+export const getEntryPageListFromLocal = async (): Promise<
+  { pageName: string; modified: string }[]
+> => {
+  try {
+    const entryPagePathList = await getEntryPagePathList();
+    const stats = entryPagePathList.map((e) => fs.stat(e));
+    const statsList = await Promise.all(stats);
+    return entryPagePathList.map((e, i) => ({
+      pageName: path.basename(e, ".mdx"),
+      modified: statsList[i].mtime.toJSON(),
+    }));
+  } catch (e) {
+    return [];
+  }
+};
