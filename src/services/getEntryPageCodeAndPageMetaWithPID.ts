3761c3f9493e15675643b992052a242c9f798a54
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 16:23:46 +0900

    キャッシュを使う

--- src/services/getEntryPageCodeAndPageMetaWithPID.ts
+++ src/services/getEntryPageCodeAndPageMetaWithPID.ts
@@ -1,16 +1,20 @@
 import { PageMeta } from "../types/PageMeta";
 import { readFileWithModifiedTime } from "../utils/readFileWithModifiedTime";
 import { sourceParser } from "../utils/parsers/sourceParser";
+import { getOrSetMDXCompileCache } from "./caches/MDXCompileCache";
 
 interface Res {
   code: string;
   pageMeta: PageMeta;
 }
 
-export const getEntryPageCodeAndPageMetaWithPID = async (
-  pid: string
-): Promise<Res> => {
-  const { src, modified } = await readFileWithModifiedTime(pid);
+const setter = (src: string, modified: string) => async (): Promise<Res> => {
   const { code, frontMatter } = await sourceParser(src);
   return { code, pageMeta: { modified, ...frontMatter } };
+};
+export const getEntryPageCodeAndPageMetaWithPID = async (
+  pid: string
+): Promise<Res | undefined> => {
+  const { src, modified } = await readFileWithModifiedTime(pid);
+  return await getOrSetMDXCompileCache(pid, modified, setter(src, modified));
 };
