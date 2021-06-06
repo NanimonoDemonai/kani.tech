22b624c73931d3079fabbf2f6e18ea3f1bb5089e
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-06 17:13:54 +0900

    いらないd.tsを消す

--- src/types/HTMLAttributes.d.ts
+++
@@ -1,6 +0,0 @@
-import { AtomicProps } from "@fower/types";
-
-declare module "react" {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-interface
-  interface HTMLAttributes<T> extends AtomicProps {}
-}
--- src/types/mdx.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "*.mdx" {
-  import type { ComponentType } from "react";
-  import type { FrontMatter } from "./FrontMatter";
-  const MDXComponent: ComponentType;
-  export const frontMatter: FrontMatter;
-  export default MDXComponent;
-}
