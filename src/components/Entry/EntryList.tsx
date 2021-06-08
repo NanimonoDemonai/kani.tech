e0867e1649d9e2c43a8ac932721c1c6e9c941bd8
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-09 00:24:36 +0900

    エントリ一覧を切り出す

---
+++ src/components/Entry/EntryList.tsx
@@ -0,0 +1,24 @@
+import { VFC } from "react";
+import { HStack, Link, ListItem, UnorderedList } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { getEntryPathWithEntryName } from "../../utils/getURL";
+import { DateTime } from "../Elements/DateTime";
+
+interface Props {
+  entryPageList: { pageName: string; modified: string }[];
+}
+
+export const EntryList: VFC<Props> = ({ entryPageList }) => (
+  <UnorderedList>
+    {entryPageList.map(({ pageName, modified }) => (
+      <ListItem key={pageName}>
+        <HStack>
+          <NextLink href={getEntryPathWithEntryName(pageName)}>
+            <Link>{pageName}</Link>
+          </NextLink>
+          <DateTime date={modified} />
+        </HStack>
+      </ListItem>
+    ))}
+  </UnorderedList>
+);
--- src/components/pages/EntryList/EntryListPage.tsx
+++ src/components/pages/EntryList/EntryListPage.tsx
@@ -1,11 +1,8 @@
 import { NextPage } from "next";
-import NextLink from "next/link";
-import { getEntryPathWithEntryName } from "../../../utils/getURL";
 import { Title } from "../../Metas/Title";
-import { HStack, Link, ListItem, UnorderedList } from "@chakra-ui/react";
 import { Article } from "../../Entry/Article";
-import { DateTime } from "../../Elements/DateTime";
 import { Tags } from "../../Elements/Tags";
+import { EntryList } from "../../Entry/EntryList";
 
 export interface EntryListProps {
   entryPageList: { pageName: string; modified: string }[];
@@ -19,18 +16,7 @@ export const EntryListPage: NextPage<EntryListProps> = ({
   <>
     <Article>
       <Title title="エントリー一覧" />
-      <UnorderedList>
-        {entryPageList.map(({ pageName, modified }) => (
-          <ListItem key={pageName}>
-            <HStack>
-              <NextLink href={getEntryPathWithEntryName(pageName)}>
-                <Link>{pageName}</Link>
-              </NextLink>
-              <DateTime date={modified} />
-            </HStack>
-          </ListItem>
-        ))}
-      </UnorderedList>
+      <EntryList entryPageList={entryPageList} />
     </Article>
     <Tags tags={tags} />
   </>
