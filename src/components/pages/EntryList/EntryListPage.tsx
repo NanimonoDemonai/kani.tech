80e8d0be27baf85e0f9aa9d5475f35263572f527
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-09 00:21:26 +0900

    タグを取得する

--- src/components/pages/EntryList/EntryListPage.tsx
+++ src/components/pages/EntryList/EntryListPage.tsx
@@ -5,25 +5,33 @@ import { Title } from "../../Metas/Title";
 import { HStack, Link, ListItem, UnorderedList } from "@chakra-ui/react";
 import { Article } from "../../Entry/Article";
 import { DateTime } from "../../Elements/DateTime";
+import { Tags } from "../../Elements/Tags";
 
 export interface EntryListProps {
   entryPageList: { pageName: string; modified: string }[];
+  tags: string[];
 }
 
-export const EntryListPage: NextPage<EntryListProps> = ({ entryPageList }) => (
-  <Article>
-    <Title title="エントリー一覧" />
-    <UnorderedList>
-      {entryPageList.map(({ pageName, modified }) => (
-        <ListItem key={pageName}>
-          <HStack>
-            <NextLink href={getEntryPathWithEntryName(pageName)}>
-              <Link>{pageName}</Link>
-            </NextLink>
-            <DateTime date={modified} />
-          </HStack>
-        </ListItem>
-      ))}
-    </UnorderedList>
-  </Article>
+export const EntryListPage: NextPage<EntryListProps> = ({
+  entryPageList,
+  tags,
+}) => (
+  <>
+    <Article>
+      <Title title="エントリー一覧" />
+      <UnorderedList>
+        {entryPageList.map(({ pageName, modified }) => (
+          <ListItem key={pageName}>
+            <HStack>
+              <NextLink href={getEntryPathWithEntryName(pageName)}>
+                <Link>{pageName}</Link>
+              </NextLink>
+              <DateTime date={modified} />
+            </HStack>
+          </ListItem>
+        ))}
+      </UnorderedList>
+    </Article>
+    <Tags tags={tags} />
+  </>
 );
