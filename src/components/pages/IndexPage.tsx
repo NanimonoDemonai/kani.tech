6577ae27b980720a097758d2483f4e4bd35c65e9
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-09 21:51:45 +0900

    ページ一覧をトップに入れておく

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 export const IndexPage: NextPage = () => (
   <article>
@@ -7,5 +8,8 @@ export const IndexPage: NextPage = () => (
       <title>kani.tech</title>
     </Head>
     <h1>カニのページ🦀</h1>
+    <Link href={"/entries"}>
+      <a>ページ一覧</a>
+    </Link>
   </article>
 );
