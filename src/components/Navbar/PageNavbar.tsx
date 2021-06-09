0e19b8f3b4461a62544149831f05feaef9ccd90c
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-10 00:36:46 +0900

    navbarに入れる

--- src/components/Navbar/PageNavbar.tsx
+++ src/components/Navbar/PageNavbar.tsx
@@ -1,15 +1,26 @@
 import { VFC } from "react";
 import NextLink from "next/link";
-import { Box, Heading, HStack, Link, Spacer } from "@chakra-ui/react";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { ExternalLinkIcon, LockIcon, UnlockIcon } from "@chakra-ui/icons";
 import { KaniButton } from "./KaniButton";
 import { CODE_REPOSITORY, SITE_NAME } from "../../constants/envNames";
 import { useRecoilState } from "recoil";
 import { titleAtoms } from "../hooks/atoms/titleAtoms";
 import { NavBarDivider } from "./NavBarDivider";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export const PageNavbar: VFC = () => {
   const [title] = useRecoilState(titleAtoms);
+  const [session, loading] = useSession();
 
   return (
     <Box>
@@ -34,6 +45,37 @@ export const PageNavbar: VFC = () => {
             <Link href={CODE_REPOSITORY.toString()} isExternal>
               Github <ExternalLinkIcon mx="2px" />
             </Link>
+            
+            {!loading && !session && (
+              <IconButton
+                colorScheme="blue"
+                aria-label="ログアウト"
+                icon={<UnlockIcon />}
+                onClick={() => {
+                  signIn();
+                }}
+                size="sm"
+              />
+            )}
+            {!loading && session && (
+              <HStack>
+                <IconButton
+                  colorScheme="blue"
+                  aria-label="ログアウト"
+                  icon={<LockIcon />}
+                  onClick={() => {
+                    signOut();
+                  }}
+                  size="sm"
+                />
+                <Avatar
+                  size="sm"
+                  name={session.user?.name || undefined}
+                  src={session.user?.image || undefined}
+                />
+                <Text fontSize="lg">{session.user?.name}</Text>
+              </HStack>
+            )}
           </HStack>
         </nav>
       </HStack>
