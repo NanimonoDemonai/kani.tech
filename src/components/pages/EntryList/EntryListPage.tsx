dd29bbc42c613dd687b8e54110ee78ae6aba071b
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-09 20:38:13 +0900

    topLevelHStackを抑制する

--- src/components/pages/EntryList/EntryListPage.tsx
+++ src/components/pages/EntryList/EntryListPage.tsx
@@ -4,6 +4,7 @@ import { Article } from "../../Entry/Article";
 import { Tags } from "../../Elements/Tags";
 import { EntryList } from "../../Entry/EntryList";
 import { EntrySummary } from "../../../types/EntrySummary";
+import { Box } from "@chakra-ui/react";
 
 export interface EntryListProps {
   entryPageList: EntrySummary[];
@@ -14,11 +15,11 @@ export const EntryListPage: NextPage<EntryListProps> = ({
   entryPageList,
   tags,
 }) => (
-  <>
+  <Box>
     <Article>
       <Title title="エントリー一覧" />
       <EntryList entryPageList={entryPageList} />
     </Article>
     <Tags tags={tags} />
-  </>
+  </Box>
 );
