19ec3d75299aa3bd0a731fb16d9a0f40fb171390
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-11 23:41:44 +0900

    エントリーをヒストリー付きで作るやつを作る

---
+++ src/services/createOrUpsertEntry.ts
@@ -0,0 +1,52 @@
+import { Entry } from "@prisma/client";
+import { prisma } from "./client/PrismClient";
+
+interface Params {
+  tags: string[];
+  source: string;
+  pageTitle: string;
+  pageName: string;
+}
+
+export const createOrUpsertEntry = async ({
+  tags,
+  source,
+  pageName,
+  pageTitle,
+}: Params): Promise<Entry> => {
+  const revisionData = (revision: number) => ({
+    source,
+    pageTitle,
+    pageName,
+    tags: {
+      connectOrCreate: tags.map((e) => ({
+        where: { tagName: e },
+        create: { tagName: e },
+      })),
+    },
+    revision,
+    history: {
+      create: [
+        {
+          revision,
+          source,
+        },
+      ],
+    },
+  });
+
+  const latest = await prisma.entry.findUnique({
+    where: { pageName },
+    select: { revision: true },
+  });
+  if (!latest) {
+    return prisma.entry.create({
+      data: revisionData(0),
+    });
+  }
+  const revision = latest.revision + 1;
+  return prisma.entry.update({
+    where: { pageName },
+    data: revisionData(revision),
+  });
+};
