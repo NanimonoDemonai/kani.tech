14395adef0cb72e50d0d972f77f5b731deafb473
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-12 02:08:26 +0900

    過去リビジョンを取得するserviceを描く

---
+++ src/services/getMDXHistorySourcePageCodeAndPageMetaWithPIDRev.ts
@@ -0,0 +1,28 @@
+import { PageMeta } from "../types/PageMeta";
+import { prisma } from "./client/PrismClient";
+
+interface Res {
+  pageMeta: PageMeta;
+}
+
+export const getMDXHistorySourcePageCodeAndPageMetaWithPIDRev = async (
+  pageName: string,
+  revision: number
+): Promise<Res | undefined> => {
+  const entry = await prisma.entry.findUnique({
+    where: { pageName },
+    include: {
+      history: { select: { source: true }, where: { revision } },
+      tags: true,
+    },
+  });
+  if (!entry) return;
+  return {
+    pageMeta: {
+      modified: entry.updatedAt.toJSON(),
+      source: entry.history[0]?.source || "",
+      title: entry.pageTitle,
+      tags: entry.tags.map((e) => e.tagName),
+    },
+  };
+};
