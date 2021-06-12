b46e3fd7bb7c3af41479b3299d43fb65f59201ed
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-12 12:56:34 +0900

    投稿ボタンを作る

--- src/components/BottomOption/MDXEditor.tsx
+++ src/components/BottomOption/MDXEditor.tsx
@@ -3,18 +3,30 @@ import MDEditor from "@uiw/react-md-editor";
 import { useRecoilState } from "recoil";
 import { pageMetaAtoms } from "../hooks/atoms/pageMetaAtoms";
 import { extractContent } from "../../utils/extractContent";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import "@uiw/react-md-editor/dist/markdown-editor.css";
 import "@uiw/react-markdown-preview/dist/markdown.css";
+import useAxios from "axios-hooks";
 
 export const MDXEditor: VFC = () => {
   const pageMeta = useRecoilState(pageMetaAtoms)[0];
   const [value, setValue] = useState<string | undefined>(
     extractContent(pageMeta?.source)
   );
+  const [{ loading, error }, refetch] = useAxios("/api/post/article", {
+    manual: true,
+  });
   return (
     <Box className="container">
       <MDEditor value={value} onChange={setValue} preview={"edit"} />
+      <Button
+        disabled={loading || !!error}
+        onClick={() => {
+          refetch({ data: { value }, method: "post" });
+        }}
+      >
+        投稿
+      </Button>
     </Box>
   );
 };
