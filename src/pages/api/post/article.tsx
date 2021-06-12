4e791ebaa26cf4c7876867f06cecce1f5a7bc64e
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-12 15:40:35 +0900

    roleにUSERを強制する

--- src/pages/api/post/article.tsx
+++ src/pages/api/post/article.tsx
@@ -1,12 +1,21 @@
-import { NextApiHandler } from "next";
+import { NextApiHandler, NextApiRequest } from "next";
 import { getSession } from "next-auth/client";
+import { Session } from "next-auth";
 
+const handleService = (session: Session, req: NextApiRequest): boolean => {
+  const user = session.user;
+  if (!user) return false;
+  const role = session.role;
+  if (role !== "USER") return false;
+
+  console.log("Session", JSON.stringify(session.user, null, 2));
+  console.log(JSON.stringify(req.body));
+  return true;
+};
 const postArticle: NextApiHandler = async (req, res) => {
   const session = await getSession({ req });
-  if (session && req.method === "POST") {
-    console.log("Session", JSON.stringify(session, null, 2));
-    console.log(JSON.stringify(req.body));
-  } else {
+
+  if (!(session && req.method === "POST" && handleService(session, req))) {
     res.status(401);
   }
   res.end();
