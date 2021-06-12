c26ef9a1bf019522b2a3c4a53ceb660dcf08287d
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-12 12:12:46 +0900

    md-editorを入れる

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+const removeImports = require("next-remove-imports")();
+module.exports = removeImports({
   pageExtensions: ["tsx"],
   future: {
     webpack5: true,
   },
-};
+});
