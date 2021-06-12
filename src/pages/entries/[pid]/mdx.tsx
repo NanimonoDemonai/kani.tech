333caa1062108ad8fe877ff43883d1b11e5974ca
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-12 11:43:23 +0900

    mdxページを消す

--- src/pages/entries/[pid]/mdx.tsx
+++
@@ -1,9 +0,0 @@
-import { MDXSourcePage } from "../../../components/pages/MDXSourcePage/MDXSourcePage";
-import { getMDXSourcePageStaticProps } from "../../../components/pages/MDXSourcePage/dataFetching";
-
-export default MDXSourcePage;
-
-export const getStaticProps = getMDXSourcePageStaticProps;
-export const getStaticPaths = async () => {
-  return { paths: [], fallback: true };
-};
