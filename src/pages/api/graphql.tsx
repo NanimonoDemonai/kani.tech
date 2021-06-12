85889a05f0a427840f096a759cab08e7a9326126
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-12 22:17:25 +0900

    書き換える

--- src/pages/api/graphql.tsx
+++ src/pages/api/graphql.tsx
@@ -1,41 +1,12 @@
-import { ApolloError, ApolloServer } from "apollo-server-micro";
+import { ApolloServer } from "apollo-server-micro";
 import typeDefs from "../../../schema.graphql";
-import { Resolvers } from "../../types/generated/graphqlCodeGen";
-import { createOrUpsertEntry } from "../../services/createOrUpsertEntry";
-import { Session } from "next-auth";
-import { getSession } from "next-auth/client";
-import { MicroRequest } from "apollo-server-micro/dist/types";
-
-interface ContextType {
-  session: Session;
-}
-
-// Provide resolver functions for your schema fields
-const resolvers: Resolvers<ContextType> = {
-  Mutation: {
-    postArticle: async (
-      parent,
-      { input: { tags, source, pageName, pageTitle } },
-      context
-    ) => {
-      if (context.session.role !== "USER") {
-        return new ApolloError("permission denied");
-      }
-      await createOrUpsertEntry({ tags, source, pageName, pageTitle });
-      return { id: "1" };
-    },
-  },
-  Query: {
-    healthCheck: () => "hello",
-  },
-};
+import { getContext } from "../../services/graphql/context";
+import { rootResolvers } from "../../services/graphql/rootResolvers";
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  context: async ({ req }: { req: MicroRequest }) => ({
-    session: await getSession({ req }),
-  }),
+  resolvers: rootResolvers,
+  context: getContext,
 });
 
 export default server.createHandler({
