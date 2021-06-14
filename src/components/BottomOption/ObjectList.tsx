0da69659f3e13545c4b1e9e27fd904a6338195b0
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-15 00:28:04 +0900

    バケツの中身を取って出すこんぽねーと

--- src/components/BottomOption/ObjectList.tsx
+++ src/components/BottomOption/ObjectList.tsx
@@ -1,17 +1,30 @@
-import { VFC } from "react";
+import { useEffect, VFC } from "react";
 import { useGetObjectListQuery } from "../../services/client/generated/graphqlCodeGen";
 import { Box, ListItem, UnorderedList } from "@chakra-ui/react";
+import { Fallback } from "../Elements/Fallback";
 
 interface Props {
   pageName: string;
+  loading: boolean;
 }
 
-export const ObjectList: VFC<Props> = ({ pageName }) => {
-  const { loading, data } = useGetObjectListQuery({
+export const ObjectList: VFC<Props> = ({ pageName, loading }) => {
+  const {
+    loading: loadingData,
+    data,
+    refetch,
+  } = useGetObjectListQuery({
     variables: { key: pageName },
   });
-  if (loading) return null;
+  useEffect(() => {
+    if (loading && loading)
+      refetch({ key: pageName }).then(() => {
+        /*noop*/
+      });
+  }, [loading, loadingData, refetch]);
+  if (loadingData) return <Fallback />;
   if (!data) return null;
+
   return (
     <Box>
       <UnorderedList>
