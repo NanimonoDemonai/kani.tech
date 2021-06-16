2873773e5fedbd73ecd694df8867642757adc722
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-16 23:38:22 +0900

    RaceCondition対策する

--- src/script/watchEntries.ts
+++ src/script/watchEntries.ts
@@ -20,14 +20,17 @@ const fileAdd = async (filePath: string) => {
   const pageName = path.basename(filePath, ".mdx");
   const file = await readFileWithModifiedTime(filePath);
   const { title: pageTitle, tags } = frontMatterParser(file.src).frontMatter;
-
-  const upsertEntry = await createOrUpsertEntry({
-    tags,
-    source: file.src,
-    pageName,
-    pageTitle,
-  });
-  console.log("upstarted", upsertEntry.pageName, upsertEntry.pageTitle);
+  try {
+    const upsertEntry = await createOrUpsertEntry({
+      tags,
+      source: file.src,
+      pageName,
+      pageTitle,
+    });
+    console.log("upstarted", upsertEntry.pageName, upsertEntry.pageTitle);
+  } catch (e) {
+    console.log(e, pageTitle, tags);
+  }
 };
 
 const fileRemove = async (filePath: string) => {
