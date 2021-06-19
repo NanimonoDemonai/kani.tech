7da2fe7421ee77ed133be059a44751b3e1b8a979
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-20 02:19:05 +0900

    prismaのクライアント切れ対策

--- src/services/client/PrismClient.ts
+++ src/services/client/PrismClient.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const _prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = _prisma;
+
+export const prisma = _prisma;
