2bde905c0cb3893b65c000159b8624dbc577bd54
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-20 01:32:18 +0900

    リビジョンの表示が崩れていた

--- src/components/BottomOption/PageRevision.tsx
+++ src/components/BottomOption/PageRevision.tsx
@@ -5,11 +5,8 @@ import { useRevision } from "../hooks/usePageMeta";
 export const PageRevision: VFC = () => {
   const revision = useRevision();
   return (
-    <>
-      revision && (
-      <Text color="gray.400" fontSize="sm">
-        revision:{revision}
-      </Text>
-    </>
+    <Text color="gray.400" fontSize="sm">
+      revision:{revision}
+    </Text>
   );
 };
