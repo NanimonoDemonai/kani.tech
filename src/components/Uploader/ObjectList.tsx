f2c49723ce16e56ee1750e177a62aa9429fbbe4a
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-20 13:40:43 +0900

    削除ボタンを表示する

--- src/components/Uploader/ObjectList.tsx
+++ src/components/Uploader/ObjectList.tsx
@@ -1,9 +1,10 @@
-import { Box, Table, Tr, Th, Thead, Tbody } from "@chakra-ui/react";
+import { Box, Table, Tr, Th, Thead, Tbody, Button } from "@chakra-ui/react";
 import { useEffect, VFC } from "react";
 import { useAsync } from "react-async-hook";
 import { gqlClient } from "../../services/client/graphqlRequest";
 import { getImageUrl } from "../../utils/getURL";
 import { noop } from "../../utils/noop";
+import { useEditorIsShown } from "../BottomOption/hooks/useEditorIsShown";
 import { Fallback } from "../Elements/Fallback";
 
 interface Props {
@@ -11,21 +12,21 @@ interface Props {
   loading: boolean;
 }
 
-export const ObjectList: VFC<Props> = ({ pageName, loading }) => {
-  const {
-    loading: loadingData,
-    result,
-    execute,
-  } = useAsync<string[]>(async () => {
+const useGetObjectList = (pageName: string) =>
+  useAsync<string[]>(async () => {
     const { getObjectList } = await gqlClient.GetObjectList({ key: pageName });
     return getObjectList;
   }, [pageName]);
+
+export const ObjectList: VFC<Props> = ({ pageName, loading }) => {
+  const { loading: loadingData, result, execute } = useGetObjectList(pageName);
+  const isEditorShown = useEditorIsShown();
   useEffect(() => {
-    if (loading && loading) execute().then(noop);
+    if (loading && loadingData) execute().then(noop);
   }, [loading, loadingData, execute, pageName]);
+
   if (loadingData) return <Fallback />;
   if (!result) return null;
-
   return (
     <Box>
       <Table variant="simple">
@@ -33,6 +34,7 @@ export const ObjectList: VFC<Props> = ({ pageName, loading }) => {
           <Tr>
             <Th>ファイル名</Th>
             <Th>サムネイル</Th>
+            {isEditorShown && <Th>編集</Th>}
           </Tr>
         </Thead>
         <Tbody>
@@ -47,6 +49,11 @@ export const ObjectList: VFC<Props> = ({ pageName, loading }) => {
                   style={{ maxWidth: 30, maxHeight: 30 }}
                 />
               </Th>
+              {isEditorShown && (
+                <Th>
+                  <Button>削除</Button>
+                </Th>
+              )}
             </Tr>
           ))}
         </Tbody>
