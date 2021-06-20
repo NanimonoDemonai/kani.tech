65379126becdab1d15a24164dbb229f7f069d456
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-20 17:16:39 +0900

    uploadFileのSlice作成

--- src/components/hooks/slices/FileUploaderSlice.ts
+++ src/components/hooks/slices/FileUploaderSlice.ts
@@ -1,28 +1,55 @@
-import { createAsyncThunk, createSlice, Reducer } from "@reduxjs/toolkit";
+import {
+  AsyncThunk,
+  createAsyncThunk,
+  createSlice,
+  Reducer,
+} from "@reduxjs/toolkit";
 import axios from "axios";
 import { gqlClient } from "../../../services/client/graphqlRequest";
 import { AsyncThunkConfig } from "../store";
 
 interface uploadFileProps {
-  pageName: string;
   file: File;
 }
 
+interface deleteFileProps {
+  key: string;
+}
+
 interface State {
+  loading: boolean;
+  deleting: boolean;
   uploading: boolean;
+  objectList: string[];
 }
 
 const initialState: State = {
+  loading: false,
+  deleting: false,
   uploading: false,
+  objectList: [],
 };
+
+export const loadObject = createAsyncThunk<string[], void, AsyncThunkConfig>(
+  "loadObject",
+  async (_, { getState }) => {
+    const {
+      pageMeta: { pageName },
+    } = getState();
+    const { getObjectList } = await gqlClient.GetObjectList({ key: pageName });
+    return getObjectList;
+  }
+);
 
 export const uploadFile = createAsyncThunk<
   void,
   uploadFileProps,
   AsyncThunkConfig
->("uploadFile", async ({ pageName, file }, { getState }) => {
-  const { Uploader } = getState();
-  if (Uploader.uploading) return;
+>("uploadFile", async ({ file }, { getState, dispatch }) => {
+  const {
+    pageMeta: { pageName },
+  } = getState();
+
   const { getUploadUrl: url } = await gqlClient.GetUploadUrl({
     contentType: file.type,
     key: `${pageName}/${file.name}`,
@@ -34,6 +61,16 @@ export const uploadFile = createAsyncThunk<
     data: file,
     headers: { "Content-Type": file.type },
   });
+  dispatch(loadObject());
+});
+
+export const deleteFile = createAsyncThunk<
+  void,
+  deleteFileProps,
+  AsyncThunkConfig
+>("deleteFile", async ({ key: key }, { dispatch }) => {
+  await gqlClient.DeleteObject({ key });
+  dispatch(loadObject());
 });
 
 const uploaderSlice = createSlice({
@@ -41,16 +78,37 @@ const uploaderSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(uploadFile.pending, (state) => {
-      state.uploading = true;
-    });
-    builder.addCase(uploadFile.fulfilled, (state) => {
-      state.uploading = false;
-    });
-    builder.addCase(uploadFile.rejected, (state) => {
-      state.uploading = false;
-    });
+    function asyncCase<T>(
+      thunk: AsyncThunk<void, T, AsyncThunkConfig>,
+      target: keyof Omit<State, "objectList">
+    ) {
+      builder
+        .addCase(thunk.pending, (state) => {
+          state[target] = true;
+        })
+        .addCase(thunk.fulfilled, (state) => {
+          state[target] = false;
+        })
+        .addCase(thunk.rejected, (state) => {
+          state[target] = false;
+        });
+    }
+
+    asyncCase(uploadFile, "uploading");
+    asyncCase(deleteFile, "deleting");
+
+    builder
+      .addCase(loadObject.pending, (state) => {
+        state.loading = true;
+        state.objectList = [];
+      })
+      .addCase(loadObject.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.objectList = payload;
+      })
+      .addCase(loadObject.rejected, (state) => {
+        state.loading = false;
+      });
   },
 });
-
 export const uploaderReducer: Reducer<State> = uploaderSlice.reducer;
