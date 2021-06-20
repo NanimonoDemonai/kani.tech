3abdcbb3424f670726df679a107b9f14c5d093e9
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-20 13:34:23 +0900

    オブジェクトリストを作る

--- src/components/Uploader/ObjectList.tsx
+++ src/components/Uploader/ObjectList.tsx
@@ -1,8 +1,9 @@
-import { Box, ListItem, UnorderedList } from "@chakra-ui/react";
+import { Box, Table, Tr, Th, Thead, Tbody } from "@chakra-ui/react";
 import { useEffect, VFC } from "react";
 import { useAsync } from "react-async-hook";
 import { gqlClient } from "../../services/client/graphqlRequest";
 import { getImageUrl } from "../../utils/getURL";
+import { noop } from "../../utils/noop";
 import { Fallback } from "../Elements/Fallback";
 
 interface Props {
@@ -20,26 +21,36 @@ export const ObjectList: VFC<Props> = ({ pageName, loading }) => {
     return getObjectList;
   }, [pageName]);
   useEffect(() => {
-    if (loading && loading) execute();
+    if (loading && loading) execute().then(noop);
   }, [loading, loadingData, execute, pageName]);
   if (loadingData) return <Fallback />;
   if (!result) return null;
 
   return (
     <Box>
-      <UnorderedList>
-        {result.map((e) => (
-          <ListItem key={e}>
-            {e}
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              src={getImageUrl(e)}
-              alt="my_skin"
-              style={{ maxWidth: 30, maxHeight: 30 }}
-            />
-          </ListItem>
-        ))}
-      </UnorderedList>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>ファイル名</Th>
+            <Th>サムネイル</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {result.map((e) => (
+            <Tr key={e}>
+              <Th>{e}</Th>
+              <Th>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={getImageUrl(e)}
+                  alt="my_skin"
+                  style={{ maxWidth: 30, maxHeight: 30 }}
+                />
+              </Th>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Box>
   );
 };
