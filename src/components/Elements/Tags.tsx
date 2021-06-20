ef9fd9bf0c775000970646c991481dd6a538f73c
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-20 17:50:34 +0900

    Tagはダイアモンドで囲む

--- src/components/Elements/Tags.tsx
+++ src/components/Elements/Tags.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { VFC } from "react";
 import { getTaggedEntryListPathWithTagName } from "../../utils/getURL";
@@ -8,11 +8,11 @@ interface Props {
 }
 
 export const Tags: VFC<Props> = ({ tags }) => (
-  <Box>
+  <>
     {tags.map((e) => (
       <NextLink href={getTaggedEntryListPathWithTagName(e)} key={e}>
         <Button size="xs">{e}</Button>
       </NextLink>
     ))}
-  </Box>
+  </>
 );
