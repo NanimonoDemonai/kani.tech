0cca5d39c0aff6c283ce365c84958a23214d9474
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-22 16:46:50 +0900

    画像アップロードするときにwidth,heightを一緒に送る

--- src/services/uploadImage.ts
+++ src/services/uploadImage.ts
@@ -1,5 +1,28 @@
 import { gqlClient } from "./client/graphqlRequest";
 
+const getImageSize = async (
+  file: File
+): Promise<{ width: number; height: number }> => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.onload = () => {
+      const size = {
+        width: img.naturalWidth,
+        height: img.naturalHeight,
+      };
+      URL.revokeObjectURL(img.src);
+      resolve(size);
+    };
+
+    img.onerror = (error) => {
+      reject(error);
+    };
+
+    img.src = URL.createObjectURL(file);
+  });
+};
+
 export const uploadImage = async (
   file: File,
   pageName: string
@@ -9,11 +32,16 @@ export const uploadImage = async (
     contentType: file.type,
     key,
   });
+  const { width, height } = await getImageSize(file);
   if (!url) return;
   await fetch(url, {
     method: "put",
     body: file,
-    headers: { "Content-Type": file.type },
+    headers: {
+      "Content-Type": file.type,
+      "x-amz-meta-width": `${width}`,
+      "x-amz-meta-height": `${height}`,
+    },
   });
   return key;
 };
