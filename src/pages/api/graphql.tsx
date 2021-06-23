acf2a8d6d99a45166ecbbb39dffef97b4b4641b2
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-23 22:04:43 +0900

    micro-graphqlをなくす

--- src/pages/api/graphql.tsx
+++ src/pages/api/graphql.tsx
@@ -1,19 +1,28 @@
 import { Benzene, makeHandler } from "@benzene/http";
 import { makeExecutableSchema } from "graphql-tools";
-import { NextApiHandler } from "next";
+import { NextApiHandler, NextApiRequest } from "next";
 import typeDefs from "../../../schema.graphql";
-import { getContext } from "../../services/graphql/context";
-import { rootResolvers } from "../../services/graphql/rootResolvers";
+import { getContext, SessionContextType } from "../../services/graphql/context";
+import {
+  AuthenticationError,
+  rootResolvers,
+} from "../../services/graphql/rootResolvers";
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers: rootResolvers,
 });
 
-const GQL = new Benzene({
+const GQL = new Benzene<SessionContextType, { req: NextApiRequest }>({
   schema,
   contextFn: async ({ extra }) => {
     return await getContext(extra);
+  },
+  formatErrorFn: (error) => {
+    if (error instanceof AuthenticationError) {
+      return error;
+    }
+    return new Error("internal error");
   },
 });
 
@@ -22,18 +31,14 @@ const graphqlHTTP = makeHandler(GQL);
 const handler: NextApiHandler = (req, res) => {
   return graphqlHTTP(
     {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      method: req.method,
+      method: req.method || "",
       headers: req.headers,
       body: req.body,
     },
     { req }
   ).then((result) => {
     for (const [key, value] of Object.entries(result.headers)) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      res.setHeader(key, value);
+      res.setHeader(key, value || "");
     }
     res.status(result.status).send(result.payload);
   });
