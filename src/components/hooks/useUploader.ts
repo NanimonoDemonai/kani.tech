a33b5bea216352d65abf5cfc418bc28e8d22afcd
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-26 16:48:31 +0900

    Uploaderをとりあえずdisableにする

--- src/components/hooks/useUploader.ts
+++ src/components/hooks/useUploader.ts
@@ -2,15 +2,16 @@ import { ImageObject } from "../../types/PageMeta";
 import { useSelector } from "./store";
 
 export const useLoading = (): boolean =>
-  useSelector((state) => state.Uploader.loading);
+  useSelector((state) => state.Uploader?.loading || true);
 
 export const useIsDisabling = (): boolean =>
-  useSelector(
-    (state) =>
-      state.Uploader.uploading ||
-      state.Uploader.deleting ||
-      state.Uploader.loading
+  useSelector((state) =>
+    state.Uploader
+      ? state.Uploader.uploading ||
+        state.Uploader.deleting ||
+        state.Uploader.loading
+      : true
   );
 
 export const useObjectList = (): ImageObject[] =>
-  useSelector((state) => state.Uploader.objectList);
+  useSelector((state) => state.Uploader?.objectList || []);
