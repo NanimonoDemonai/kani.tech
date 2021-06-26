3a8f5e53526f38f2f8e0112c43d174196cd34ec4
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-27 00:25:36 +0900

    BottomSourceをLazyloadさす

--- src/components/BottomOption/BottomOptionComponents/BottomSource.tsx
+++ src/components/BottomOption/BottomOptionComponents/BottomSource.tsx
@@ -1,15 +1,26 @@
 import { Collapse } from "@chakra-ui/react";
-import { VFC } from "react";
-import { useSource } from "../../hooks/useMDXEditor";
+import { useEffect, useState, VFC } from "react";
+import { Fallback } from "../../Elements/Fallback";
+import { useSource } from "../../hooks/usePageMeta";
 import { useIsBottomOptionShowSource } from "../../hooks/usePageOption";
 import { DynamicSourceHighlighter } from "./DynamicSourceHighlighter";
 
 export const BottomSource: VFC = () => {
   const isBottomOptionShowSource = useIsBottomOptionShowSource();
   const source = useSource();
+
+  const [initialized, setInitialized] = useState(false);
+  useEffect(() => {
+    if (isBottomOptionShowSource) setInitialized(true);
+  }, [isBottomOptionShowSource]);
+
   return (
     <Collapse in={isBottomOptionShowSource} animateOpacity>
-      <DynamicSourceHighlighter source={source} />
+      {initialized ? (
+        <DynamicSourceHighlighter source={source} />
+      ) : (
+        <Fallback />
+      )}
     </Collapse>
   );
 };
