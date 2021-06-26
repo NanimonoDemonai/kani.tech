8e06ad90d58d5c4710e0e60b9e603ff866cdf91c
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-27 00:43:31 +0900

    ssrしない

--- src/components/BottomOption/BottomOptionComponents/BottomEditor.tsx
+++ src/components/BottomOption/BottomOptionComponents/BottomEditor.tsx
@@ -3,8 +3,9 @@ import { VFC } from "react";
 import { useIsBottomOptionShowEditor } from "../../hooks/usePageOption";
 import { DynamicCollapse } from "./DynamicCollapse";
 
-const DynamicMDXEditor = dynamic<unknown>(() =>
-  import("../../MDXEditor/MDXEditor").then((mod) => mod.MDXEditor)
+const DynamicMDXEditor = dynamic<unknown>(
+  () => import("../../MDXEditor/MDXEditor").then((mod) => mod.MDXEditor),
+  { ssr: false }
 );
 export const BottomEditor: VFC = () => {
   const editorIsOpen = useIsBottomOptionShowEditor();
--- src/components/BottomOption/BottomOptionComponents/BottomImageUploader.tsx
+++ src/components/BottomOption/BottomOptionComponents/BottomImageUploader.tsx
@@ -3,8 +3,9 @@ import { VFC } from "react";
 import { useIsBottomOptionFileListEditor } from "../../hooks/usePageOption";
 import { DynamicCollapse } from "./DynamicCollapse";
 
-const DynamicImageUploader = dynamic<unknown>(() =>
-  import("../../Uploader/ImageUploader").then((mod) => mod.ImageUploader)
+const DynamicImageUploader = dynamic<unknown>(
+  () => import("../../Uploader/ImageUploader").then((mod) => mod.ImageUploader),
+  { ssr: false }
 );
 
 export const BottomImageUploader: VFC = () => {
