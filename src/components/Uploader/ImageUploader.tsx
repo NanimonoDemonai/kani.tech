a997169349497a714991fa9a7435505ea939ff78
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-26 16:51:06 +0900

    Uploaderをlazyにする

--- src/components/Uploader/ImageUploader.tsx
+++ src/components/Uploader/ImageUploader.tsx
@@ -1,10 +1,14 @@
 import { Stack, Text } from "@chakra-ui/react";
 import { VFC } from "react";
+import { uploaderReducer } from "../hooks/slices/FileUploaderSlice";
+import { useInjectReducer } from "../hooks/store";
 import { useEditorIsShown } from "../hooks/useEditorIsShown";
 import { ImageUploaderDropzone } from "./ImageUploaderDropzone";
 import { ObjectList } from "./ObjectList";
 
 export const ImageUploader: VFC = () => {
+  useInjectReducer({ Uploader: uploaderReducer });
+
   const editorIsShown = useEditorIsShown();
 
   return (
