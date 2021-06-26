13f2713953c8c0b3c1184e0df3353f173b0716c0
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-26 21:51:24 +0900

    entrylistを動かす

--- src/pages/entries/index.tsx
+++ src/pages/entries/index.tsx
@@ -1,5 +1,5 @@
 import { EntryListPage } from "../../components/pages/EntryList/EntryListPage";
-import { getEntryPageListStaticProps } from "../../components/pages/EntryList/dataFetching";
+import { getEntryPageListStaticProps } from "../../services/dataFetcher/getEntryPageListStaticProps";
 
 export const getStaticProps = getEntryPageListStaticProps;
 export default EntryListPage;
