9d16ab150ba349ba9ffe982ef4968ef9f79627ec
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-06-27 21:58:47 +0900

    DocumentBucketを定数化

--- src/constants/s3Bucket.ts
+++ src/constants/s3Bucket.ts
@@ -1,2 +1,5 @@
 export const Bucket: string =
   process.env.NEXT_PUBLIC_AWS_S3_BUCKET || "defaultBucket";
+
+export const DocumentBucket: string =
+  process.env.AWS_S3_DOCUMENT_BUCKET || "defaultDocumentBucket";
