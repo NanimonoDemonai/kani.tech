c1ae7f61f6f2f2c97848bb7100a05438e35bd159
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-07-01 13:27:56 +0900

    今まで分けていたところを合わせる

--- src/constants/NextSecretEnv.ts
+++ src/constants/NextSecretEnv.ts
@@ -1,4 +1,4 @@
-export const revalidate = 3600;
-export const uploaderTimeout = 10;
+export const revalidate = process.env.PAGE_REVALIDATE_TIME || 3600;
+export const uploaderTimeout = process.env.PAGE_UPLOADER_TIMEOUT_MIN || 10;
 export const DocumentBucket: string =
   process.env.AWS_S3_DOCUMENT_BUCKET || "defaultDocumentBucket";
