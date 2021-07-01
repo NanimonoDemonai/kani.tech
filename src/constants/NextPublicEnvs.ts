1c71946d375868df9367c52151fcd0955c72f588
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-07-01 13:25:58 +0900

    secretとpublicを分ける

--- src/constants/NextPublicEnvs.ts
+++ src/constants/NextPublicEnvs.ts
@@ -3,3 +3,5 @@ export const CODE_REPOSITORY = new URL(
   process.env.NEXT_PUBLIC_CODE_REPOSITORY ||
     "https://github.com/NanimonoDemonai/kani.tech"
 );
+export const Bucket: string =
+  process.env.NEXT_PUBLIC_AWS_S3_BUCKET || "defaultBucket";
--- src/constants/NextSecretEnv.ts
+++ src/constants/NextSecretEnv.ts
@@ -1,5 +1,4 @@
-export const Bucket: string =
-  process.env.NEXT_PUBLIC_AWS_S3_BUCKET || "defaultBucket";
-
+export const revalidate = 3600;
+export const uploaderTimeout = 10;
 export const DocumentBucket: string =
   process.env.AWS_S3_DOCUMENT_BUCKET || "defaultDocumentBucket";
