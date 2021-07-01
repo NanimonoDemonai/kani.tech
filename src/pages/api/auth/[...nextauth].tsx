962822081d79560f012fa4d064db77c38c89159e
nanimono demonai <masa9nao5.n4.18@gmail.com>
2021-07-01 13:49:50 +0900

    nextAuthもずらす

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,30 +1,3 @@
-import NextAuth from "next-auth";
-import { Prisma } from "next-auth/adapters";
-import Providers from "next-auth/providers";
-import { prisma } from "../../../services/backend/client/PrismClient";
+import { NextAuthHandler } from "../../../services/backend/authentication/NextAuthHandler";
 
-export default NextAuth({
-  providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id as string,
-          name: profile.name || (profile.login as string),
-          email: `${profile.id}+${profile.login}@users.noreply.github.com`,
-          image: profile.avatar_url as string,
-        };
-      },
-    }),
-  ],
-  adapter: Prisma.Adapter({ prisma }),
-  callbacks: {
-    session: (session, userOrToken) => {
-      const user = {
-        ...session.user,
-      };
-      return { ...session, user, role: userOrToken.role };
-    },
-  },
-});
+export default NextAuthHandler;
---
+++ src/services/backend/authentication/NextAuthHandler.ts
@@ -0,0 +1,30 @@
+import NextAuth from "next-auth";
+import { Prisma } from "next-auth/adapters";
+import Providers from "next-auth/providers";
+import { prisma } from "../client/PrismClient";
+
+export const NextAuthHandler = NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id as string,
+          name: profile.name || (profile.login as string),
+          email: `${profile.id}+${profile.login}@users.noreply.github.com`,
+          image: profile.avatar_url as string,
+        };
+      },
+    }),
+  ],
+  adapter: Prisma.Adapter({ prisma }),
+  callbacks: {
+    session: (session, userOrToken) => {
+      const user = {
+        ...session.user,
+      };
+      return { ...session, user, role: userOrToken.role };
+    },
+  },
+});
